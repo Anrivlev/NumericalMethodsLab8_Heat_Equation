c4b77e33f7c7373b3fbd39b4afc20f32a11bc7e8
Andrey Ivlev <warcraft1921@gmail.com>
2022-05-08 17:19:55 +0300

    order of approximation is checked

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def initial_conditions1():
     alpha = np.array([1, 1])
     beta = np.array([1, 0])
     gamma = np.array([lambda t: np.tan(t) - 0.5, lambda t: (np.e * np.tan(t) - 1) / 2])
-    u0 = lambda x, t: (np.exp(x)*np.tan(t) - x) / 2 # exact solution
+    u0 = lambda x, t: (np.exp(x)*np.tan(t) - x) / 2  # exact solution
     return a, sigma, phi, f, alpha, beta, gamma, u0
 
 
@@ -26,13 +26,14 @@ def initial_conditions2():
     # alpha[1] * u(1, t) + beta[1] * u_x(1, t) = gamma[1](t) | right border condition
     a = 1.
     sigma = 0.5
-    phi = lambda x: 0
-    f = lambda x, t: np.exp(x) / 2 * (1 / np.cos(t) / np.cos(t) - np.tan(t))
-    alpha = np.array([1, 1])
-    beta = np.array([1, 0])
-    gamma = np.array([lambda t: np.tan(t) - 0.5, lambda t: (np.e * np.tan(t) - 1) / 2])
-    u0 = lambda x, t: 0
+    phi = lambda x: x
+    f = lambda x, t: 0
+    alpha = np.array([0, 0])
+    beta = np.array([1, 1])
+    gamma = np.array([lambda t: 0, lambda t: 0])
+    u0 = lambda x, t: 0  # exact solution
     return a, sigma, phi, f, alpha, beta, gamma, u0
+
 
 def least_squares(x, y):
     n = len(x)
@@ -80,11 +81,11 @@ def next_layer_first_order(u_prev, a2, sigma, func, alpha, beta, gamma, t_now, t
         c[i] = coef * sigma
         f[i] = -u_prev[i] + coef * (sigma - 1) * (u_prev[i + 1] - 2 * u_prev[i] + u_prev[i - 1]) - tau*func(i * h, t_now - tau/2)
     a[0] = 0.
-    b[0] = -alpha[0] / h + beta[0]
-    c[0] = alpha[0] / h
+    b[0] = -beta[0] / h + alpha[0]
+    c[0] = beta[0] / h
     f[0] = gamma[0](t_now)
-    a[-1] = -alpha[1] / h
-    b[-1] = alpha[1] / h + beta[1]
+    a[-1] = -beta[1] / h
+    b[-1] = beta[1] / h + alpha[1]
     c[-1] = 0.
     f[-1] = gamma[1](t_now)
     u_next = solve3diagonal(a, b, c, f)
@@ -105,7 +106,7 @@ def animation():
     N = int((x_max - x_min) // h)  # number of points
     x_range = np.linspace(x_min, x_max, N)
 
-    a, sigma, phi, f, alpha, beta, gamma, u0 = initial_conditions1()
+    a, sigma, phi, f, alpha, beta, gamma, u0 = initial_conditions2()
     next_layer = next_layer_first_order
 
     u = np.zeros((2, N))
@@ -142,5 +143,45 @@ def animation():
     anim.save('solution.gif', writer='imagemagick')
 
 
+def order_of_approximation():
+    x_min = 0.  # left border
+    x_max = 1.  # right border
+    t_min = 0.  # initial time
+    Nt = 50  # number of time steps
+    h_range = np.array([0.00025, 0.0005, 0.001, 0.002, 0.0025, 0.005, 0.01])
+    next_layer = next_layer_first_order
+    a, sigma, phi, f, alpha, beta, gamma, u0 = initial_conditions1()
+
+    error = np.zeros(len(h_range))
+
+    for h, i in zip(h_range, range(len(h_range))):
+        print(h)
+        N = int((x_max - x_min) // h)  # number of points
+        x_range = np.linspace(x_min, x_max, N)
+        tau = h
+        t_max = t_min + tau * Nt
+        t_range = np.linspace(t_min + 2 * tau, t_max, Nt - 1)
+        u = np.zeros((2, N))
+        u[0] = phi(x_range)
+        u[1] = next_layer(u[0], a, sigma, f, alpha, beta, gamma, tau, tau, h)
+        for t in t_range:
+            u[0] = u[1]
+            u[1] = next_layer(u[0], a, sigma, f, alpha, beta, gamma, t, tau, h)
+            error[i] = max(error[i], np.max(np.abs(u[1] - u0(x_range, t))))
+
+    plt.suptitle('Зависимость логарифма абсолютной погрешности от логарифма шага интегрирования')
+    plt.subplot(1, 1, 1)
+    plt.xlabel("log(h)")
+    plt.ylabel("log(max(|Δu|))")
+    plt.grid()
+    plt.loglog(h_range, error, color='k')
+
+    coeffs = least_squares(np.log10(h_range), np.log10(error))
+    print("linear regression", ": y(x) = ", coeffs[0], " + ", coeffs[1], "x", sep="")
+
+    plt.show()
+
+
 if __name__ == '__main__':
-    animation()
+    order_of_approximation()  # Call it in order to check the order of approximation
+    # animation()  # Call it in order to draw solution.gif
